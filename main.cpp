ab5f36c30e54fbd7474f34e872b49fa0a16a8b42
Michael Heron <40605443+mikeheroncode@users.noreply.github.com>
2019-10-02 18:41:39 -0400

    Added additional test cases for new classes of Vehicles

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include "Car.h"
 #include "Bicycle.h"
+#include "Jet.h"
+#include "Skateboard.h"
+#include "TemperamentalHorse.h"
 
 void printVehiclesRoster(Vehicle **vehicles, int size);
 
 int main() {
     std::cout << "Driving simulator" << std::endl;
     int size = 6;
-    int capacity = 10;
+    int capacity = 12;
     Vehicle **vehiclesArray = new Vehicle *[capacity];
 
     vehiclesArray[0] = new Car();
@@ -16,6 +19,12 @@ int main() {
     vehiclesArray[3] = new Car("Tesla", "T2", "electricity", "large");
     vehiclesArray[4] = new Bicycle("Mizuno", "Wave", 10);
     vehiclesArray[5] = new Car("BMW", "X5", "diesel", "grande");
+    vehiclesArray[6] = new Jet("Boeing", "747", "Rocket", 8);
+    vehiclesArray[7] = new Jet("Airbus", "BJX76", "diesel", 4);
+    vehiclesArray[8] = new Skateboard("Element", "Coolguy");
+    vehiclesArray[9] = new Skateboard("Birdhouse", "RadBoard");
+    vehiclesArray[10] = new TemperamentalHorse("Mustang", "Stallion", "good");
+    vehiclesArray[11] = new TemperamentalHorse("Destrier", "Mare");
 
     printVehiclesRoster(vehiclesArray, size);
 
